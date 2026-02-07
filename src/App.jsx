25769f9612b291e157a074016c5806ee5a98f784
SUDHANSHU GHOSH <shudhanshukumar9713@gmail.com>
2026-02-07 15:48:06 +0530

    add intro video

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,29 @@
+import { useState } from 'react'
 import Quiz from './page/Quiz/Quiz'
+import introVideo from './assets/intro2.mp4'
 
 const App = () => {
+  // State to track if the intro video is playing
+  const [isPlaying, setIsPlaying] = useState(true)
+
   return (
-    <Quiz />
+    <>
+      {isPlaying ? (
+        <div className="intro-video-container">
+          <video 
+            autoPlay 
+            muted 
+            playsInline 
+            onEnded={() => setIsPlaying(false)} // Hides video when it finishes
+            className="intro-video"
+          >
+            <source src={introVideo} type="video/mp4" />
+          </video>
+        </div>
+      ) : (
+        <Quiz />
+      )}
+    </>
   )
 }
 
